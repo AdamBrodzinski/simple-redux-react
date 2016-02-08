3b7bc904f9018554c21a760651f57a24dd665027
Adam Brodzinski <adambrodzinski@gmail.com>
2016-02-07 22:54:47 -0500

    remove log

--- src/devtools.js
+++ src/devtools.js
@@ -3,8 +3,6 @@ import { createDevTools } from 'redux-devtools'
 import LogMonitor from 'redux-devtools-log-monitor'
 import DockMonitor from 'redux-devtools-dock-monitor'
 
-console.log('devtools')
-
 export const DevTools = createDevTools(
   <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
     <LogMonitor theme="tomorrow" preserveScrollTop={false} />
